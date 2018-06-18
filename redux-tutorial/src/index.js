ed5eeb748c2fde4f8ea9b5e7fc65429d961919cc
Server <november14giel@gmail.com>
2018-06-17 22:50:57 -0400

    state and action, dispatcher

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -4,5 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//modified code here
+import { createStore } from 'redux';
+
+//reducer
+  function reducer(state, action) {
+    console.log("this is action 💪 ",action)
+    return 'hello 🌎';
+  }
+const store = createStore(reducer);
+console.log(store.getState())
+
+//dispatching an action
+const action = {
+  type: 'changeState',
+  payload: {
+    newState: 'New state'
+  }
+}
+//set action to the store
+store.dispatch(action);
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
